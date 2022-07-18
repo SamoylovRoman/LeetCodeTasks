236537cb18162019a2d08f7b4c3fe575ce6d5bb1
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-18 20:44:15 +0700

    Merge Sorted Array commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/MergeSortedArray.kt
@@ -0,0 +1,28 @@
+package com.codelabs.leetcodeproject
+
+class MergeSortedArray {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        for ((j, i) in (m until m + n).withIndex()) {
+            nums1[i] = nums2[j]
+        }
+        var tmp: Int
+        for (i in nums1.indices) {
+            for (j in i + 1 until nums1.size) {
+                if (nums1[i] > nums1[j]) {
+                    tmp = nums1[j]
+                    nums1[j] = nums1[i]
+                    nums1[i] = tmp
+                }
+            }
+        }
+        nums1.forEach {
+            println(it)
+        }
+    }
+
+}
+
+fun main() {
+    val newMerge = MergeSortedArray()
+    newMerge.merge(intArrayOf(1, 2, 3, 0, 0, 0), 3, intArrayOf(2, 5, 6), 3)
+}
