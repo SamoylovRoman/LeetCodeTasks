0f390f393bc6ebcbef02e8da353effb6192013a1
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-18 19:45:57 +0700

    mergeTwoLists commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/MergeTwoLists.kt
@@ -0,0 +1,49 @@
+package com.codelabs.leetcodeproject
+
+import java.util.*
+import kotlin.math.min
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class MergeTwoLists {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        val result: ListNode? = ListNode(0)
+        var current = result
+
+        var node1 = list1
+        var node2 = list2
+        while (node1 != null || node2 != null) {
+            if (node1 == null) {
+                current?.next = node2
+                break
+            }
+            if (node2 == null) {
+                current?.next = node1
+                break
+            }
+
+            if (node1.`val` < node2.`val`) {
+                current?.next = node1
+                node1 = node1.next
+            } else {
+                current?.next = node2
+                node2 = node2.next
+            }
+            current = current?.next
+        }
+        if (node1 != null) {
+            current?.next = node1
+        } else {
+            current?.next = node2
+        }
+        return result?.next
+    }
+}
+
+
+fun main() {
+    var li = ListNode(5)
+    val list: LinkedList<String>
+}
