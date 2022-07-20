feeb0caba518d20195423aac643666a0b4dcc581
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-20 21:41:24 +0700

    14. Longest Common Prefix commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/LongestCommonPrefix.kt
@@ -0,0 +1,25 @@
+package com.codelabs.leetcodeproject
+
+class LongestCommonPrefix {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        val word = strs[0]
+        var pref = ""
+        for (i in word.indices) {
+            pref += word[i]
+            strs.forEach {
+                if (!it.startsWith(pref)) {
+                    return pref.dropLast(1)
+                }
+            }
+        }
+        return pref
+    }
+}
+
+fun main() {
+    val obj = LongestCommonPrefix()
+    println(obj.longestCommonPrefix(arrayOf("c", "acc", "ccc")))
+    println(obj.longestCommonPrefix(arrayOf("flower", "flow", "flight")))
+    println(obj.longestCommonPrefix(arrayOf("dog", "racecar", "car")))
+    println(obj.longestCommonPrefix(arrayOf("a")))
+}
