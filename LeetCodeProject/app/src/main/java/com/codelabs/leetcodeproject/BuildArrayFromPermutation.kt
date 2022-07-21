8e6f7681c33ee9cc6df8338f8ef6950002d54a82
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-21 17:25:25 +0700

    1920. Build Array from Permutation commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/BuildArrayFromPermutation.kt
@@ -0,0 +1,20 @@
+package com.codelabs.leetcodeproject
+
+class BuildArrayFromPermutation {
+    fun buildArray(nums: IntArray): IntArray {
+        return nums.map {
+            nums[it]
+        }.toIntArray()
+    }
+}
+
+fun main() {
+    val obj = BuildArrayFromPermutation()
+    obj.buildArray(intArrayOf(0,2,1,5,3,4)).forEach {
+        print("$it ")
+    }
+    println()
+    obj.buildArray(intArrayOf(5,0,1,2,3,4)).forEach {
+        print("$it ")
+    }
+}
