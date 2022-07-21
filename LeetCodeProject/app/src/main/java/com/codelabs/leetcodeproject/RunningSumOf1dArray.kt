9e82064a929625e66b054233fe2507328e8326e6
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-21 17:57:29 +0700

    1480. Running Sum of 1d Array commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/RunningSumOf1dArray.kt
@@ -0,0 +1,28 @@
+package com.codelabs.leetcodeproject
+
+class RunningSumOf1dArray {
+    fun runningSum(nums: IntArray): IntArray {
+        var sum = 0
+        nums.forEachIndexed { index, i ->
+            sum += i
+            nums[index] = sum
+        }
+        return nums
+    }
+}
+
+fun main() {
+    val obj = RunningSumOf1dArray()
+    obj.runningSum(intArrayOf(1, 2, 3, 4)).forEach {
+        print("$it ")
+    }
+    println()
+    obj.runningSum(intArrayOf(1, 1, 1, 1, 1)).forEach {
+        print("$it ")
+    }
+    println()
+    obj.runningSum(intArrayOf(3, 1, 2, 10, 1)).forEach {
+        print("$it ")
+    }
+    println()
+}
