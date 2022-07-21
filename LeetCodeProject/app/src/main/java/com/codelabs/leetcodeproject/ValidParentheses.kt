46b7781140899fb124e892f6f6e53e85ca6ab691
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-21 12:12:12 +0700

    20. Valid Parentheses commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/ValidParentheses.kt
@@ -0,0 +1,59 @@
+package com.codelabs.leetcodeproject
+
+class ValidParentheses {
+    fun isValid(s: String): Boolean {
+        var flag = true
+        val stack = mutableListOf<Char>()
+        s.forEach {
+            when (it) {
+                '(' -> stack.add(0, it)
+                ')' -> if (stack.isEmpty()) {
+                    flag = false
+                    return@forEach
+                } else {
+                    if (stack[0] == '(') {
+                        stack.removeAt(0)
+                    } else {
+                        flag = false
+                        return@forEach
+                    }
+                }
+                '{' -> stack.add(0, it)
+                '}' -> if (stack.isEmpty()) {
+                    flag = false
+                    return@forEach
+                } else {
+                    if (stack[0] == '{') {
+                        stack.removeAt(0)
+                    } else {
+                        flag = false
+                        return@forEach
+                    }
+                }
+                '[' -> stack.add(0, it)
+                ']' -> if (stack.isEmpty()) {
+                    flag = false
+                    return@forEach
+                } else {
+                    if (stack[0] == '[') {
+                        stack.removeAt(0)
+                    } else {
+                        flag = false
+                        return@forEach
+                    }
+                }
+            }
+        }
+        return flag && stack.isEmpty()
+    }
+}
+
+fun main() {
+    val obj = ValidParentheses()
+    println(obj.isValid("]"))
+    println(obj.isValid("()"))
+    println(obj.isValid("{[]}")) //true
+    println(obj.isValid("()[]{}"))
+    println(obj.isValid("(]"))
+    println(obj.isValid("([)]"))
+}
