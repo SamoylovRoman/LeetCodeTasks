d15d7eabf0bc8b654d8e481946bbdd5f50e0f513
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-27 10:08:59 +0700

    709. To Lower Case commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/easy/ToLowerCase.kt
@@ -0,0 +1,12 @@
+package com.codelabs.leetcodeproject.easy
+
+class ToLowerCase {
+    fun toLowerCase(s: String): String {
+        return s.toLowerCase()
+    }
+}
+
+fun main() {
+    val obj = ToLowerCase()
+    println(obj.toLowerCase("HelLo"))
+}
