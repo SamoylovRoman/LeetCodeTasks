26dc30c1f47ac9e140b80207ef4a443314abeacc
Roman Samoylov <samoylov.r.v@gmail.com>
2022-08-04 08:49:46 +0700

    2000. Reverse Prefix of Word commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/easy/ReversePrefixOfWord.kt
@@ -0,0 +1,26 @@
+package com.codelabs.leetcodeproject.easy
+
+class ReversePrefixOfWord {
+    fun reversePrefix(word: String, ch: Char): String {
+        var str = ""
+        val index = word.indexOfFirst {
+            it == ch
+        }
+        if (index == -1) {
+            return word
+        } else {
+            str += word.substring(0, index + 1).reversed()
+            if (index != word.length) {
+                str += word.substring(index + 1, word.length)
+            }
+        }
+        return str
+    }
+}
+
+fun main() {
+    val obj = ReversePrefixOfWord()
+    println(obj.reversePrefix("abcdefd", 'd'))
+    println(obj.reversePrefix("xyxzxe", 'z'))
+    println(obj.reversePrefix("abcd", 'z'))
+}
