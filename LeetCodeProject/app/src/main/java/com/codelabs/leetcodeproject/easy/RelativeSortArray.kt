dcde6e36a80ccc3fab34a2f70306caa0e1d8acb8
Roman Samoylov <samoylov.r.v@gmail.com>
2022-08-30 09:12:49 +0700

    1122. Relative Sort Array commit

---
+++ LeetCodeProject/app/src/main/java/com/codelabs/leetcodeproject/easy/RelativeSortArray.kt
@@ -0,0 +1,24 @@
+package com.codelabs.leetcodeproject.easy
+
+class RelativeSortArray {
+    fun relativeSortArray(arr1: IntArray, arr2: IntArray): IntArray {
+        val list = mutableListOf<Int>()
+        arr2.forEach { value ->
+            for (i in 0 until arr1.count { it == value }) list += value
+        }
+        list += (arr1.toList() - arr2.toList().toSet()).sorted()
+        return list.toIntArray()
+    }
+}
+
+fun main() {
+    val obj = RelativeSortArray()
+    obj.relativeSortArray(
+        intArrayOf(2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19),
+        intArrayOf(2, 1, 4, 3, 9, 6)
+    ).forEach { print("$it ") }
+    println()
+    obj.relativeSortArray(intArrayOf(28, 6, 22, 8, 44, 17), intArrayOf(22, 28, 8, 6))
+        .forEach { print("$it ") }
+    println()
+}
